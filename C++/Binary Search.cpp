a9e11a3279fe6a02b67e3b56b0019f817a8db6d5
Ananya Prasad <71277914+ananyap18@users.noreply.github.com>
2020-10-18 19:52:51 +0530

    Update Binary Search.cpp

--- C++/Binary Search.cpp
+++ C++/Binary Search.cpp
@@ -1,27 +1,32 @@
-#include<iostream>
-using namespace std;
-int binarysearch(int arr[], int num, int start, int end)
-{   while(start<=end){
-    int middle = (start+end)/2;
-        if(arr[middle] == num)
-           return middle;
-        if(arr[middle]>num)
-           end=middle-1;
-        else
-           start=middle+1;
-       
-           }
-   return -1;
+#include<iostream.h>
+#include<conio.h>
+void bubblesort(int arr[],int size)
+{	int temp;
+	for(int i=0;i<size;i++)
+	{	for(int j=0;j<size-1;j++)
+		{	if(arr[j]>arr[j+1])
+			{	temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+}
+void main()
+{	clrscr();
+	int a[50],s;
+	cout<<"\n\t\tEnter size of the array:";
+	cin>>s;
+	cout<<"\n\t\tEnter the records into the array:\n\t\t";
+	for(int i=0;i<s;i++)
+	       {	cin>>a[i];
+			cout<<"\n\t\t";
+	       }
+	cout<<"\n\t\tSorted array is as follows:\n";
+	bubblesort(a,s);
+	for(int n=0;n<s;n++)
+	       {	cout<<a[n]<<"\n\t\t";
+	       }
+
 }
 
-int main()
-{
-   int arr[]={1,2,6,7,8,10};
-   int num;
-   cin>>num;
-   int n = sizeof(arr)/ sizeof(arr[0]);
-   int ans=binarysearch(arr,num,0,n-1);
-   cout<<ans;
-
-   return 0;
-}
